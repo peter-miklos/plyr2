6646a6ceaec88012ae1db465ca1ca47ef014eb0f
Peter Miklos <pt.miklos@gmail.com>
2016-10-25 17:58:10 +0100

    users.js updated

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV ? process.env.NODE_ENV : process.env.NODE_ENV = 'development';
-
 var express = require('express');
 var router = express.Router();
 var firebase = require("firebase");
