f5e951042908d64db247e50c26d6b6389be12e65
Stéphan Gaud <stephgoeseast@gmail.com>
2016-10-25 12:51:31 +0100

    event page tests done

--- test/features/events.js
+++ test/features/events.js
@@ -2,7 +2,7 @@ var app = require('../../app');
 var Browser = require('zombie');
 var http = require('http');
 
-xdescribe('events page content', function() {
+describe('events page content', function() {
 
   before(function(){
     server = http.createServer(app).listen(3000);
@@ -10,34 +10,36 @@ xdescribe('events page content', function() {
   });
 
   before(function(done) {
-    browser.visit('/');
+    browser.visit('/', done);
+  });
+
+  before(function(done) {
     browser.pressButton('Create Event', done);
   });
 
-  it('should be succefull', function() {
-    browser.assert.success();
-  });
-  after(function(done) {
-    server.close(done);
+  it('should allow you to create new events', function() {
+    browser.assert.url({pathname: '/events/new'});
   });
 
   describe('create a new event', function() {
 
-    before(function(){
-      server = http.createServer(app).listen(3000);
-      browser = new Browser({ site: 'http://localhost:3000'});
+    before(function(done){
+      browser.visit('/events/new', done);
     });
 
     before(function(done) {
-      browser.visit('/events').then(function(){
-      browser
-      .select('sport', "wresling")
-      .check('master')
-      .fill('event_date', 2016-10-27)
-      .select('time', "15:00")
-      .fill('comment', "I dare you!")
-      .pressButton('Create Event!', done);
-      });
+      browser.select('sport', "wrestling", done);
+      browser.check('master', done);
+      browser.fill('event_date', 2016-10-27, done);
+      browser.select('time', "15:00", done);
+      browser.fill('comment', "I dare you!", done);
+      browser.pressButton('Create Event!', done);
+    });
+    it("should redirect to homepage", function() {
+      browser.assert.url({pathname: '/'});
+    });
+    it("should display wrestling on the page", function() {
+       browser.assert.text("body", /wrestling/);
     });
   });
 
