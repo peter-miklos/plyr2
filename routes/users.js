1e3072e2e64281ab93941b33a61be37f7e88bf64
Alfie <alfie_ab@hotmail.co.uk>
2016-10-25 11:13:36 +0100

    first stages, receiving 500 error

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+
+  res.render(path.join(__dirname, '../views', 'users/logIn.ejs'));
+
 });
 
 module.exports = router;
