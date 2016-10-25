6af521125f91d63b7b7234ec51bc9de1849cffac
Peter Miklos <pt.miklos@gmail.com>
2016-10-25 13:49:55 +0100

    firebase added, sessions fixed

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var sess;
+var session = require('express-session');
+var firebase = require("firebase");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
