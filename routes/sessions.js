4d1c674183604b1747413005820f62b10a679d51
ercekal <ercekalabalikoglu@gmail.com>
2016-10-26 11:04:02 +0100

    corrects the routing

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/new', function(req, res, next) {
-res.render('events/new', {  title: "Create event" });
+  res.render('login', { title: 'User Login' });
 });
 
 router.post('/', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -4,27 +4,27 @@ var router = express.Router();
 /* GET users listing. */
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup', {title: "User signup"});
+  res.render('users/signup', {title: "User signup"});
 });
 
 router.post('/signup', function(req, res, next) {
   sess = req.session;
   if(req.body.password && req.body.password === req.body.pwd_confirm) {
     firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).catch(function(error) {
-      console.log("Error: " + error.code + " - " + error.message)
+      console.log("Error: " + error.code + " - " + error.message);
     }).then(function() {
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           sess.current_user = user;
         } else {
-          console.log("User is not assigned to the session variable")
+          console.log("User is not assigned to the session variable");
         }
-      })
+      });
     });
     res.redirect('/');
   } else {
-    console.log("Password confirmation does not match")
-    res.redirect("/users/signup")
+    console.log("Password confirmation does not match");
+    res.redirect("/users/signup");
   }
 
 
@@ -35,7 +35,7 @@ router.post('/login', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', {
+  res.render('sessions/login', {
     title: "User login"
   });
 });
