9695e62c1ca231caa9af1de8e6d63c720c55d749
Peter Miklos <pt.miklos@gmail.com>
2016-10-27 17:39:25 +0100

    events related test cases, events/new view updated

--- test/features/events.js
+++ test/features/events.js
@@ -12,8 +12,15 @@ describe('Manage events', function() {
    browser = new Browser({ site: 'http://localhost:3000'});
  });
 
- describe("Create new event", function() {
+ describe("Creates new event", function() {
    before(function(done){
+    //  models.Event.create({
+    //    skill: 3,
+    //    eventDate: "2016-10-30",
+    //    eventTime: "15:00",
+    //    location: "London",
+    //    description: "description"
+    //  });
      browser.visit('/events/new', done);
    });
 
@@ -23,7 +30,7 @@ describe('Manage events', function() {
      browser.fill('date', '2016-10-30', done);
      browser.fill('time', "15:00", done);
      browser.fill('location', "Peter's house", done);
-     browser.fill('comment', "I dare you!", done);
+     browser.fill('description', "I dare you!", done);
      browser.pressButton('Create Event!', done);
    });
 
@@ -37,13 +44,53 @@ describe('Manage events', function() {
    });
  });
 
- describe("Inform user if there is no events found", function() {
+ describe("Informs user if there is no events found", function() {
    before(function(done){
      browser.visit('/events/index', done);
    });
 
    it("Message is shown on the page", function() {
       browser.assert.text("body", /No events found/);
+   });
+ })
+
+ describe("Shows events in a list", function() {
+   before(function(done){
+     browser.visit('/events/new').then(function() {
+       browser
+        .select("#sport", "Wrestling")
+        .select("#skill", "4")
+        .fill('date', '2016-11-30')
+        .fill('time', "15:00")
+        .fill('location', "Hyde Park")
+        .fill('description', "I dare you!")
+        .pressButton('Create Event!', done);
+     })
+   });
+
+   it("Message is shown on the page", function() {
+      browser.assert.text("body", /Hyde Park/);
+   });
+ })
+
+ describe("Shows events in a list", function() {
+   before(function(done){
+     browser.visit('/events/new').then(function() {
+       browser
+        .select("#sport", "Wrestling")
+        .select("#skill", "4")
+        .fill('date', '2016-11-30')
+        .fill('time', "15:00")
+        .fill('location', "Hyde Park")
+        .fill('description', "wrestling in the Hyde Park")
+        .pressButton('Create Event!', done);
+     }).then(function() {
+       browser.clickLink("Wed Nov 30 2016", done);
+     })
+   });
+
+   it("Shows the content of the event", function() {
+      browser.assert.text("body", /wrestling in the Hyde Park/);
    });
  })
 
