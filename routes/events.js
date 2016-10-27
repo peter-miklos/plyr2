92a09659933ab40e283a662e0aa2d56975863dd9
ercekal <ercekalabalikoglu@gmail.com>
2016-10-27 12:43:59 +0100

    models have issue but we're on good track

--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('index')
+router.get('/index', function(req, res, next) {
+  res.render('index');
 });
 
 router.get('/new', function(req, res, next) {
-  res.render('events/new', {  title: "Create event" });
+  res.render('events/new', {  title: "Create new event" });
+});
+
+router.post('/new', function(req, res, next) {
+  models.Event.create({
+    skill: req.body.skill,
+    date: req.body.date,
+    time: req.body.time,
+    location: req.body.location,
+    comment: req.body.comment
+  }).then(function(user) {
+    res.json(user);
+  });
+  res.redirect('/index');
 });
 
 router.post('/', function(req, res, next) {
