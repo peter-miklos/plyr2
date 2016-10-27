fd22917154ba8c693e065fa5ecc1cdf58ac9e763
Peter Miklos <pt.miklos@gmail.com>
2016-10-27 23:40:15 +0100

    Login test updated

--- test/features/login_test.js
+++ test/features/login_test.js
@@ -10,40 +10,51 @@ describe('log in', function() {
   before(function(){
     server = http.createServer(app).listen(3000);
     browser = new Browser({ site: 'http://localhost:3000'});
-  });
-
-
-  before(function(done) {
     models.sequelize.sync({force:true})
-    browser.visit('/', done);
   });
 
-  before(function(done) {
-    browser.clickLink('Log in', done);
-  });
+  before(function() {
+    models.User.create({
+      name: "Test User",
+      email: "test1@test.com",
+      password: "$2a$10$eZNtx.H4tr28cCQDRofbVuePFNnpxUwMcAt0HLQhB5rBzONL0wVne"
+    })
+  })
 
-  it('should allow you to log in', function() {
-    browser.assert.url({pathname: '/users/login'});
-  });
-
-  describe('user log in', function() {
-
+  describe("user login page available", function() {
     before(function(done) {
-      browser.visit('/users/login', done);
+      browser.visit('/', done);
     });
 
     before(function(done) {
-      browser.fill('email', 'ewan@hotmail.com', done);
-      browser.fill('password', 'Password', done);
-      browser.clickLink('Log in', done)
+      browser.clickLink('Log in', done);
+    });
+
+    it('should allow you to log in', function() {
+      browser.assert.url({pathname: '/sessions/login'});
+    });
+  })
+
+  describe('user logs in', function() {
+    before(function(done) {
+      browser.visit('/sessions/login', done);
+    });
+
+    before(function(done) {
+      browser
+        .fill('email', 'test1@test.com')
+        .fill('password', 'Password')
+        .clickLink('Log in', done)
     });
 
     it("should redirect to homepage if sign up is successful", function(){
       browser.assert.url({pathname: '/'});
     });
 
+    it("should show the user's name in the navbar", function(){
+      browser.assert.text("nav", /Test User/);
+    });
   });
-
 
   after(function(done) {
     server.close(done);
