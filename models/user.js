33f1549dc466320ff505049bcb8e958270ad5dfd
Alfie <alfie_ab@hotmail.co.uk>
2016-10-27 17:16:13 +0100

    password now encrypted

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 'use strict';
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.TEXT
+    password: {
+      type: DataTypes.TEXT,
+        set:  function(v) {
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(v, salt);
+
+            this.setDataValue('password', hash);
+        }
+      }
   }, {
     classMethods: {
       associate: function(models) {
