193d293cf6ba374d21b7fd4c5cde21841c653e8b
Peter Miklos <pt.miklos@gmail.com>
2016-10-28 16:33:00 +0100

    requests/index is available

--- routes/sessions.js
+++ routes/sessions.js
@@ -32,13 +32,32 @@ router.get('/logout', function(req, res) {
 });
 
 router.get('/:id/requests/index', function(req, res, next) {
-  var myRequests;
-  var receivedRequests;
-  models.Request.findAll({where: {UserId: req.session.user.id}}).then(function(requests) {
-    myRequests = requests
+  models.Sport.findAll({}).then(function(sports) {
+    models.Event.findAll({}).then(function(events) {
+      models.Request.findAll({where: {UserId: req.session.user.id}}).then(function(myRequests) {
+        models.Status.findAll({}).then(function(statuses) {
+          models.Event.findAll({where: {UserId: req.session.user.id}}).then(function(myEvents) {
+            models.User.findAll({}).then(function(users) {
+              models.Request.findAll({where: {
+                  EventId: {
+                    $any: myEvents.map(function(elem) { return elem.id })
+                  }
+              }}).then(function(receivedRequests) {
+                res.render("events/requests/index", {myRequests: myRequests,
+                                                    receivedRequests: receivedRequests,
+                                                    title1: "Received requests",
+                                                    title2: "Sent requests",
+                                                    events: events,
+                                                    sports: sports,
+                                                    statuses: statuses,
+                                                    users: users})
+              })
+            })
+          })
+        })
+      })
+    })
   })
-  // receivedRequests must be found and rendered to the view
-  res.render("events/requests/index", {myRequests: myRequests, receivedRequests: receivedRequests})
 })
 
 module.exports = router;
