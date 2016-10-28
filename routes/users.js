bdfc6bf8a4630877069d57920bcba90a7db18816
Stéphan Gaud <stephgoeseast@gmail.com>
2016-10-28 13:44:52 +0100

    still some work to do around partials

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var models = require("../models")
+var models = require("../models");
 var session = require('express-session');
 var user;
 
 router.get('/signup', function(req, res, next) {
-  res.render('users/signup', {title: "User signup"});
+  res.render('users/signup', {title: "User signup", flash: req.flash('userCreated')});
 });
 
 router.post('/signup', function(req, res, next) {
@@ -15,6 +15,7 @@ router.post('/signup', function(req, res, next) {
                     password: req.body.password,
                     password_confirmation: req.body.password_confirmation})
   .then(function (user) {
+    req.flash('userCreated', 'User created');
     req.session.user = user;
     res.redirect('/');
   })
