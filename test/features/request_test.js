84c05551fe260ec15c318621bccfc5ebd7dc817e
Peter Miklos <pt.miklos@gmail.com>
2016-10-29 23:21:49 +0100

    request creation related test cases updated

--- test/features/request_test.js
+++ test/features/request_test.js
@@ -19,8 +19,8 @@ describe('Manage requests', function() {
 
   before(function(done) {
     browser
-      .fill('name', 'Ewan')
-      .fill('email', 'ewan@hotmail.com')
+      .fill('name', 'Test User 1')
+      .fill('email', 'test1@test.com')
       .fill('password', 'Password')
       .fill('password_confirmation', 'Password')
       .pressButton('Create Account', done);
@@ -72,7 +72,7 @@ describe('Manage requests', function() {
 
       it("User doesn't see the my requests", function() {
         browser.clickLink("Log out").then(function() {
-          browser.assert.hasNoClass("li", "test");
+          browser.assert.hasNoClass("li", "my_requests");
         })
       })
     })
@@ -88,19 +88,91 @@ describe('Manage requests', function() {
       });
 
       it("User cannot create a request if not logged in", function() {
-
+        browser.assert.hasNoClass("div", "join_button");
       })
-
-
     });
 
-    describe("User cannot create a request to his/her event", function() {
+    describe("an event and a connected request cannot have the same owner", function() {
+      before(function(done){
+        browser.visit('/').then(function() {
+          browser.clickLink("Log in", done);
+        })
+      });
+
+      before(function(done) {
+        browser
+          .fill('email', 'test1@test.com')
+          .fill('password', 'Password')
+          .pressButton('Log in', done);
+      });
+
+      before(function(done){
+        browser.visit('/events/index').then(function() {
+          browser.clickLink("Wed Nov 30 2016", done);
+        })
+      });
+
+      it("Join button not visible at logged in user's event", function() {
+        browser.assert.hasNoClass("div", "join_button");
+      })
+    });
+
+    describe("User is able to join an event", function() {
+
+      before(function(done){
+        browser.visit("/").then(function() {
+          browser.clickLink("Log out");
+        }).then(function() {
+          browser.visit('/users/signup', done);
+        })
+      });
+
+      before(function(done) {
+        browser
+          .fill('name', 'Test User2')
+          .fill('email', 'test2@test.com')
+          .fill('password', 'Password')
+          .fill('password_confirmation', 'Password')
+          .pressButton('Create Account', done);
+      });
+
+      before(function(done){
+        browser.visit('/events/index').then(function() {
+          browser.clickLink("Wed Nov 30 2016", done);
+        })
+      });
+
+      it("Join button visible at another user's event", function() {
+        browser.assert.elements("button", { atLeast: 1 });
+      })
 
     });
 
     describe("User can create a request to an event", function() {
 
-    })
+      before(function(done){
+        browser.visit('/events/index').then(function() {
+          browser.clickLink("Wed Nov 30 2016", done);
+        })
+      });
+
+      before(function(done) {
+        browser.pressButton("Join", done);
+      })
+
+      it("request's empty comment field is available", function() {
+        browser.assert.elements("textarea", { atLeast: 1 });
+      })
+
+      it("created request is available in user's sent requests", function(done) {
+        browser.fill("comment", "I really wanna play", done);
+        browser.pressButton("Send request!").then(function() {
+          browser.assert.text("div", /I really wanna play/);
+        }).then(function() {
+          done();
+        })
+      })
+    });
 
   });
 
