d5dd648b0afc97e94e0e6473572f2eeaa47bb6f2
supasuma <supasuma@gmail.com>
2016-10-30 14:00:05 +0000

    connecting stylesheet & minor form styling

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -2,8 +2,46 @@
 
 $(document).ready(function() {
 
-  $(".clickable-row").click(function() {
-    window.document.location = $(this).data("href");
-  });
+  if(document.location.href.indexOf("/events/index") != -1) {
+    $("tr.clickable-row").find("a").click(function() {
+      showHideEvent(this);
+      return false;
+    });
+
+    $(".clickable-row").click(function() {
+      showHideEvent($(this).find("a"));
+    });
+  }
+  else {
+    //what pages was the below code intended for, can I get rid of it?
+    $(".clickable-row").click(function() {
+      window.document.location = $(this).data("href");
+    });
+  }
+
+  function showEvent(aTag) {
+    var pageUrl = $(aTag).attr("href");
+    var parentTr = $(aTag).parent().parent();
+
+    $.get( pageUrl, function( data ) {
+      parentTr.after('<tr><td colspan="6"><table>' + $(data).find(".table").html() + '</table></td></tr>');
+      $(aTag).attr('class', 'expanded');
+    });
+  }
+
+  function showHideEvent(aTag) {
+    if ($(aTag).attr('class') == 'expanded') {
+      $(aTag).parent().parent().next().hide();
+      $(aTag).attr('class', 'collapsed');
+    }
+    else if ($(aTag).attr('class') == 'collapsed') {
+      $(aTag).parent().parent().next().show();
+      $(aTag).attr('class', 'expanded');
+    }
+    else
+    {
+      showEvent(aTag);
+    }
+  }
 
 })
