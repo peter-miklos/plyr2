69c9ebc89229b44d18e1612420586c1dca7d998a
supasuma <supasuma@gmail.com>
2016-10-31 17:32:06 +0000

    add logic for lat and long

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -10,17 +10,17 @@ $(document).ready(function() {
     function initialize() {
       var input = document.getElementById('searchTextField');
       var autocomplete = new google.maps.places.Autocomplete(input);
-      }
-
       google.maps.event.addListener(autocomplete, 'place_changed', function () {
             var place = autocomplete.getPlace();
-            document.getElementById('city2').value = place.name;
             document.getElementById('cityLat').value = place.geometry.location.lat();
             document.getElementById('cityLng').value = place.geometry.location.lng();
-            alert("This function is working!");
-            alert(place.name);
-            alert(place.address_components[0].long_name);
+            // alert(place.name);
+            // alert(place.geometry.location.lng());
+            // alert(place.geometry.location.lat());
            });
+      }
+
+
       google.maps.event.addDomListener(window, 'load', initialize);
   }
 
