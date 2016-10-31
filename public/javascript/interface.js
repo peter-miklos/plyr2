48a084f29ea1917a58f801c80d0f9ef4e96fd7a2
supasuma <supasuma@gmail.com>
2016-10-31 14:15:58 +0000

    moved api logic to interface.js and env file

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
     window.location = $(this).data("href");
   });
 
+  if(document.location.href.indexOf("/events/new") != -1) {
+    function initialize() {
+      var input = document.getElementById('searchTextField');
+      var autocomplete = new google.maps.places.Autocomplete(input);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+  }
+
 })
