bad7f9287afe9e1115c5a085fe0ffb5be6ae90e1
BenRoss92 <benjaminjackross@gmail.com>
2016-10-31 10:56:30 +0000

    default time shown when creating event

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -18,4 +18,17 @@ $(document).ready(function() {
 
 document.getElementById('event_date').valueAsDate = new Date();
 
+$(function(){
+   $("#event_time").each(function(){
+     var d = new Date(),
+         h = d.getHours(),
+         m = d.getMinutes();
+     if(h < 10) h = '0' + h;
+     if(m < 10) m = '0' + m;
+     $(this).attr({
+       'value': h + 1 + ':' + m
+     });
+   });
+ });
+
 })
