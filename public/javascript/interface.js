ea2b185a27d67d06a99fb8dd090a2ca801fe0a55
supasuma <supasuma@gmail.com>
2016-10-31 15:09:56 +0000

    playing

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -11,6 +11,16 @@ $(document).ready(function() {
       var input = document.getElementById('searchTextField');
       var autocomplete = new google.maps.places.Autocomplete(input);
       }
+
+      google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            var place = autocomplete.getPlace();
+            document.getElementById('city2').value = place.name;
+            document.getElementById('cityLat').value = place.geometry.location.lat();
+            document.getElementById('cityLng').value = place.geometry.location.lng();
+            alert("This function is working!");
+            alert(place.name);
+            alert(place.address_components[0].long_name);
+           });
       google.maps.event.addDomListener(window, 'load', initialize);
   }
 
