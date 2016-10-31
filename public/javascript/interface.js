822d373dda1b899c2fa594c9d7194dd4d9ba3cb1
Peter Miklos <pt.miklos@gmail.com>
2016-10-31 07:23:08 +0000

    public folder and events/index view updated

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
   $(".clickable-row").click(function() {
     window.location = $(this).data("href");
   });
-
-
-  $("tr").not(':first').hover(
-    function () {
-      $(this).css("background","aqua");
-    },
-    function () {
-      $(this).css("background","");
-    }
-  );
+  //
+  //
+  // $("tr").not(':first').hover(
+  //   function () {
+  //     $(this).css("background","aqua");
+  //   },
+  //   function () {
+  //     $(this).css("background","");
+  //   }
+  // );
 
 })
