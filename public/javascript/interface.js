f7a0b6ce27b96b19ed882857a5687133ee7ce6e2
BenRoss92 <benjaminjackross@gmail.com>
2016-10-31 14:46:13 +0000

    default date and time - tests now pass

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -14,30 +14,29 @@ $(document).ready(function() {
       google.maps.event.addDomListener(window, 'load', initialize);
   }
 
-//   var now = new Date();
-//
-//   var day = ("0" + now.getDate()).slice(-2);
-//   var month = ("0" + (now.getMonth() + 1)).slice(-2);
-//
-//   var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
-//
-//
-//  $('#event_date').val(today);
-//
-// var today = new Date().toISOString().split('T')[0];
-//
-// $(function(){
-//    $("#event_time").each(function(){
-//      var d = new Date(),
-//          h = d.getHours(),
-//          m = d.getMinutes();
-//      if(h < 10) h = '0' + h;
-//      if(m < 10) m = '0' + m;
-//      $(this).attr({
-//        'value': h + 1 + ':' + m,
-//        'min': h + 1 + ':' + m
-//      });
-//    });
-//  });
+var now = new Date();
 
-})
+ var day = ("0" + now.getDate()).slice(-2);
+ var month = ("0" + (now.getMonth() + 1)).slice(-2);
+
+ var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+
+
+$('#event_date').val(today);
+$('#event_date').attr({'min': today});
+
+$(function(){
+  $("#event_time").each(function(){
+    var d = new Date(),
+        h = d.getHours(),
+        m = d.getMinutes();
+    if(h < 10) h = '0' + h;
+    if(m < 10) m = '0' + m;
+    $(this).attr({
+      'value': h + 1 + ':' + m,
+      'min': h + 1 + ':' + m
+    });
+  });
+});
+
+});
