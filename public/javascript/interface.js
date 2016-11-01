9bf6676b3120027b2c8bf7174ab9b4400d6aea45
Peter Miklos <pt.miklos@gmail.com>
2016-11-01 22:45:53 +0000

    filter function refactored, events/index view updated

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -2,40 +2,9 @@
 
 $(document).ready(function() {
 
-
-  $("#searchInput").keyup(function () {
-      //split the current value of searchInput
-      var data = this.value.toUpperCase().split(" ");
-      //create a jquery object of the rows
-      var jo = $("#fbody").find("tr");
-      if (this.value == "") {
-          jo.show();
-          return;
-      }
-      //hide all the rows
-      jo.hide();
-
-      //Recusively filter the jquery object to get results.
-      jo.filter(function (i, v) {
-          var $t = $(this);
-          for (var d = 0; d < data.length; ++d) {
-              if ($t.text().toUpperCase().indexOf(data[d]) > -1) {
-                  return true;
-              }
-          }
-          return false;
-      })
-      //show the rows that match.
-      .show();
-  }).focus(function () {
-      this.value = "";
-      $(this).css({
-          "color": "black"
-      });
-      $(this).unbind('focus');
-  }).css({
-      "color": "#C0C0C0"
-  });
+  if (document.getElementById("searchInput")) {
+    filterEventDate();
+  }
 
   if (document.getElementById("map")) {
     getEventLocations();
@@ -52,6 +21,42 @@ $(document).ready(function() {
   $(".clickable-row").click(function() {
     window.location = $(this).data("href");
   });
+
+  function filterEventDate() {
+    $("#searchInput").keyup(function () {
+        //split the current value of searchInput
+        var data = this.value.toUpperCase().split(" ");
+        //create a jquery object of the rows
+        var jo = $("#fbody").find("tr");
+        if (this.value == "") {
+            jo.show();
+            return;
+        }
+        //hide all the rows
+        jo.hide();
+
+        //Recusively filter the jquery object to get results.
+        jo.filter(function (i, v) {
+            var $t = $(this);
+            for (var d = 0; d < data.length; ++d) {
+                if ($t.text().toUpperCase().indexOf(data[d]) > -1) {
+                    return true;
+                }
+            }
+            return false;
+        })
+        //show the rows that match.
+        .show();
+    }).focus(function () {
+        this.value = "";
+        $(this).css({
+            "color": "black"
+        });
+        $(this).unbind('focus');
+    }).css({
+        "color": "#C0C0C0"
+    });
+  }
 
   function calculateCoordinates() {
     if(document.location.href.indexOf("/events/new") != -1) {
