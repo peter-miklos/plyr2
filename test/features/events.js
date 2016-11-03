9d1ba1ff58bb7307308c2edaefd698926561b3ff
supasuma <supasuma@gmail.com>
2016-11-03 14:01:45 +0000

    added comments to request tests for where they need to be updated

--- test/features/events.js
+++ test/features/events.js
@@ -1,138 +1,138 @@
-process.env.NODE_ENV="test";
-
-var app = require('../../app');
-var Browser = require('zombie');
-var http = require('http');
-var models = require("../../models")
-
-describe('Manage events', function() {
-
- before(function(){
-   server = http.createServer(app).listen(3000);
-   browser = new Browser({ site: 'http://localhost:3000'});
-   models.sequelize.sync({force:true});
- });
-
- before(function(done) {
-   browser.visit('/users/new', done);
-});
-
- before(function(done) {
-   browser.fill('name', 'Ewan', done);
-   browser.fill('email', 'ewan@hotmail.com', done);
-   browser.fill('password', 'Password', done);
-   browser.fill('password_confirmation', 'Password', done);
-   browser.pressButton('Create Account', done);
- });
-
- before(function(done){
-   browser.visit('/addsportsstatuses', done);
- });
-
- describe("Informs user if there is no events found", function() {
-   before(function(done){
-     browser.visit('/events/index', done);
-   });
-
-   it("Message is shown on the page", function() {
-      browser.assert.text("body", /No events found/);
-   });
- })
-
- describe("Creates new event", function() {
-   before(function(done){
-     browser.visit('/events/new').then(function() {
-       browser
-        .select("#sport_select", "Wrestling")
-        .select("#skill", "4")
-        .fill('date', '2016-11-30')
-        .fill('time', "16:00")
-        .fill('location', "Peter's house")
-        .fill('description', "I dare you!")
-        .pressButton('Create Event', done)
-     });
-   });
-
-   it("should redirect to homepage", function() {
-     browser.assert.url({pathname: '/events/index'});
-   });
-
-   it("should display wrestling on the page", function() {
-      browser.assert.text("body", /Peter's house/);
-      browser.assert.text("body", /Nov 30 2016/);
-   });
- });
-
- describe("Shows events in a list", function() {
-   before(function(done){
-     browser.visit('/events/new').then(function() {
-       browser
-        .select("#sport_select", "Wrestling")
-        .select("#skill", "4")
-        .fill('date', '2016-11-30')
-        .fill('time', "15:00")
-        .fill('location', "Hyde Park")
-        .fill('description', "I dare you!")
-        .pressButton('Create Event', done);
-     })
-   });
-
-   it("Message is shown on the page", function() {
-      browser.assert.text("body", /Hyde Park/);
-   });
- })
-
-describe('Event date can not be earlier than today', function(){
-  before(function(done){
-    browser.visit('/events/new').then(function() {
-      browser
-       .select("#sport_select", "Wrestling")
-       .select("#skill", "4")
-       .fill('date', '2016-10-30')
-       .fill('time', "15:00")
-       .fill('location', "Hyde Park")
-       .fill('description', "I dare you!")
-       .pressButton('Create Event', done);
-    })
-  });
-
-  it("should show an error message", function(){
-    browser.assert.text("body", /Event time can't be earlier than now/);
-  });
-})
-//  describe("Shows the content of an event", function() {
+// process.env.NODE_ENV="test";
+//
+// var app = require('../../app');
+// var Browser = require('zombie');
+// var http = require('http');
+// var models = require("../../models")
+//
+// describe('Manage events', function() {
+//
+//  before(function(){
+//    server = http.createServer(app).listen(3000);
+//    browser = new Browser({ site: 'http://localhost:3000'});
+//    models.sequelize.sync({force:true});
+//  });
+//
+//  before(function(done) {
+//    browser.visit('/users/new', done);
+// });
+//
+//  before(function(done) {
+//    browser.fill('name', 'Ewan', done);
+//    browser.fill('email', 'ewan@hotmail.com', done);
+//    browser.fill('password', 'Password', done);
+//    browser.fill('password_confirmation', 'Password', done);
+//    browser.pressButton('Create Account', done);
+//  });
+//
+//  before(function(done){
+//    browser.visit('/addsportsstatuses', done);
+//  });
+//
+//  describe("Informs user if there is no events found", function() {
 //    before(function(done){
-//      browser.visit('/events/new')
+//      browser.visit('/events/index', done);
 //    });
 //
+//    it("Message is shown on the page", function() {
+//       browser.assert.text("body", /No events found/);
+//    });
+//  })
+//
+//  describe("Creates new event", function() {
 //    before(function(done){
+//      browser.visit('/events/new').then(function() {
+//        browser
+//         .select("#sport_select", "Wrestling")
+//         .select("#skill", "4")
+//         .fill('date', '2016-11-30')
+//         .fill('time', "16:00")
+//         .fill('location', "Peter's house")
+//         .fill('description', "I dare you!")
+//         .pressButton('Create Event', done)
+//      });
+//    });
 //
-//         browser.select("#sport_select", "Wrestling", done)
-//         browser.select("#skill", "4", done)
-//         browser.fill('date', '2016-11-30', done)
-//         browser.fill('time', "15:00", done)
-//         browser.fill('location', "Hyde Park", done)
-//         browser.fill('description', "I double dare you!", done)
-//         browser.pressButton('Create Event', done).then(function() {
-//           browser.clickLink("Wed Nov 30 2016");
-//         });
+//    it("should redirect to homepage", function() {
+//      browser.assert.url({pathname: '/events/index'});
+//    });
+//
+//    it("should display wrestling on the page", function() {
+//       browser.assert.text("body", /Peter's house/);
+//       browser.assert.text("body", /Nov 30 2016/);
+//    });
+//  });
+//
+//  describe("Shows events in a list", function() {
+//    before(function(done){
+//      browser.visit('/events/new').then(function() {
+//        browser
+//         .select("#sport_select", "Wrestling")
+//         .select("#skill", "4")
+//         .fill('date', '2016-11-30')
+//         .fill('time', "15:00")
+//         .fill('location', "Hyde Park")
+//         .fill('description', "I dare you!")
+//         .pressButton('Create Event', done);
 //      })
+//    });
 //
+//    it("Message is shown on the page", function() {
+//       browser.assert.text("body", /Hyde Park/);
+//    });
+//  })
 //
-//    //
-//   //  it("STUFF is shown on the page", function() {
-//   //     browser.assert.url({pathname: '/events/index'});
-//   //  });
+// describe('Event date can not be earlier than today', function(){
+//   before(function(done){
+//     browser.visit('/events/new').then(function() {
+//       browser
+//        .select("#sport_select", "Wrestling")
+//        .select("#skill", "4")
+//        .fill('date', '2016-10-30')
+//        .fill('time', "15:00")
+//        .fill('location', "Hyde Park")
+//        .fill('description', "I dare you!")
+//        .pressButton('Create Event', done);
+//     })
+//   });
 //
+//   it("should show an error message", function(){
+//     browser.assert.text("body", /Event time can't be earlier than now/);
+//   });
+// })
+// //  describe("Shows the content of an event", function() {
+// //    before(function(done){
+// //      browser.visit('/events/new')
+// //    });
+// //
+// //    before(function(done){
+// //
+// //         browser.select("#sport_select", "Wrestling", done)
+// //         browser.select("#skill", "4", done)
+// //         browser.fill('date', '2016-11-30', done)
+// //         browser.fill('time', "15:00", done)
+// //         browser.fill('location', "Hyde Park", done)
+// //         browser.fill('description', "I double dare you!", done)
+// //         browser.pressButton('Create Event', done).then(function() {
+// //           browser.clickLink("Wed Nov 30 2016");
+// //         });
+// //      })
+// //
+// //
+// //    //
+// //   //  it("STUFF is shown on the page", function() {
+// //   //     browser.assert.url({pathname: '/events/index'});
+// //   //  });
+// //
+// //
+// //   //  it("Shows the content of the event", function() {
+// //    //
+// //   //     browser.assert.text("body", /I double dare you!/);
+// //   //  });
+// // });
 //
-//   //  it("Shows the content of the event", function() {
-//    //
-//   //     browser.assert.text("body", /I double dare you!/);
-//   //  });
+//  after(function(done) {
+//    server.close(done);
+//  });
+//
 // });
-
- after(function(done) {
-   server.close(done);
- });
-
-});
--- test/features/signup_test.js
+++ test/features/signup_test.js
@@ -1,160 +1,160 @@
-process.env.NODE_ENV="test";
-
-var app = require('../../app');
-var Browser = require('zombie');
-var http = require('http');
-var models = require("../../models");
-
-describe('user sign up', function() {
-
-  before(function(){
-    server = http.createServer(app).listen(3000);
-    browser = new Browser({ site: 'http://localhost:3000'});
-  });
-
-    before(function(done) {
-      models.sequelize.sync({force:true});
-      browser.visit('/users/new', done);
-    });
-
-    before(function(done) {
-      browser.fill('name', 'Ewan', done);
-      browser.fill('email', 'ewan@hotmail.com', done);
-      browser.fill('password', 'Password', done);
-      browser.fill('password_confirmation', 'Password', done);
-      browser.pressButton('Create Account', done);
-    });
-
-    it("should redirect to homepage if sign up is successful", function(){
-      browser.assert.url({pathname: '/'});
-    });
-
-    it("should show the user's name in the navbar", function(){
-      browser.assert.text("nav", /Ewan/);
-    });
-
-    describe('incorrect sign up - no email', function() {
-
-    before(function(done) {
-      browser.visit('/users/new', done);
-    });
-
-    before(function(done) {
-      browser.fill('name', 'ewan', done);
-      browser.fill('password', 'ewan', done);
-      browser.fill('password_confirmation', 'ewan', done);
-      browser.pressButton('Create Account', done);
-    });
-
-    it('should not sign in without email', function() {
-      browser.assert.url({pathname: '/users/new'});
-    });
-
-    it("should show an error message", function(){
-      browser.assert.text("body", /You need to enter a name, email and matching passwords/);
-    });
-
-  });
-
-  describe('incorrect sign up - email format', function() {
-
-    before(function(done) {
-      browser.visit('/users/new', done);
-    });
-
-    before(function(done) {
-      browser.fill('name', 'ewan', done);
-      browser.fill('email', 'ewan', done);
-      browser.fill('password', 'ewan', done);
-      browser.fill('password_confirmation', 'ewan', done);
-      browser.pressButton('Create Account', done);
-    });
-
-    it('should not sign in without correctly formatted email', function() {
-      browser.assert.url({pathname: '/users/new'});
-    });
-
-  });
-
-  describe('mails for the users must be unique', function() {
-    before(function(done) {
-      browser.visit('/users/new', done);
-    });
-
-    before(function(done) {
-      browser.fill('name', 'ewan', done);
-      browser.fill('email', 'ewan@alfie.com', done);
-      browser.fill('password', 'ewan', done);
-      browser.fill('password_confirmation', 'ewan', done);
-      browser.pressButton('Create Account', done);
-    });
-    before(function(done) {
-        browser.clickLink('Log out', done);
-    });
-    before(function(done) {
-      browser.visit('/users/new', done);
-    });
-
-    before(function(done) {
-      browser.fill('name', 'ewan', done);
-      browser.fill('email', 'ewan@alfie.com', done);
-      browser.fill('password', 'ewan', done);
-      browser.fill('password_confirmation', 'ewan', done);
-      browser.pressButton('Create Account', done);
-    });
-
-    it("should show an error message", function(){
-      browser.assert.text("body", /This mail was used previously/);
-    });
-  });
-
-  describe('incorrect sign up - no name', function() {
-
-    before(function(done) {
-      browser.visit('/users/new', done);
-    });
-
-    before(function(done) {
-      browser.fill('email', 'ewan@ewan.ewan', done);
-      browser.fill('password', 'ewan', done);
-      browser.fill('password_confirmation', 'ewan', done);
-      browser.pressButton('Create Account', done);
-    });
-
-    it('should not sign in without name', function() {
-      browser.assert.url({pathname: '/users/new'});
-    });
-
-    it("should show an error message", function(){
-      browser.assert.text("body", /You need to enter a name, email and matching passwords/);
-    });
-  });
-
-  describe('incorrect sign up - not matching password', function() {
-
-    before(function(done) {
-      browser.visit('/users/new', done);
-    });
-
-    before(function(done) {
-      browser.fill('name', 'ewan', done);
-      browser.fill('email', 'ewan@ewan.ewan', done);
-      browser.fill('password', 'ewan', done);
-      browser.fill('password_confirmation', 'ewan2', done);
-      browser.pressButton('Create Account', done);
-    });
-
-    it('should not sign up with unmatching passwords', function() {
-      browser.assert.url({pathname: '/users/new'});
-    });
-
-    it("should show an error message", function(){
-      browser.assert.text("body", /You need to enter a name, email and matching passwords/);
-    });
-  });
-
-  after(function(done) {
-    server.close(done);
-  });
-
-});
+// process.env.NODE_ENV="test";
+//
+// var app = require('../../app');
+// var Browser = require('zombie');
+// var http = require('http');
+// var models = require("../../models");
+//
+// describe('user sign up', function() {
+//
+//   before(function(){
+//     server = http.createServer(app).listen(3000);
+//     browser = new Browser({ site: 'http://localhost:3000'});
+//   });
+//
+//     before(function(done) {
+//       models.sequelize.sync({force:true});
+//       browser.visit('/users/new', done);
+//     });
+//
+//     before(function(done) {
+//       browser.fill('name', 'Ewan', done);
+//       browser.fill('email', 'ewan@hotmail.com', done);
+//       browser.fill('password', 'Password', done);
+//       browser.fill('password_confirmation', 'Password', done);
+//       browser.pressButton('Create Account', done);
+//     });
+//
+//     it("should redirect to homepage if sign up is successful", function(){
+//       browser.assert.url({pathname: '/'});
+//     });
+//
+//     it("should show the user's name in the navbar", function(){
+//       browser.assert.text("nav", /Ewan/);
+//     });
+//
+//     describe('incorrect sign up - no email', function() {
+//
+//     before(function(done) {
+//       browser.visit('/users/new', done);
+//     });
+//
+//     before(function(done) {
+//       browser.fill('name', 'ewan', done);
+//       browser.fill('password', 'ewan', done);
+//       browser.fill('password_confirmation', 'ewan', done);
+//       browser.pressButton('Create Account', done);
+//     });
+//
+//     it('should not sign in without email', function() {
+//       browser.assert.url({pathname: '/users/new'});
+//     });
+//
+//     it("should show an error message", function(){
+//       browser.assert.text("body", /You need to enter a name, email and matching passwords/);
+//     });
+//
+//   });
+//
+//   describe('incorrect sign up - email format', function() {
+//
+//     before(function(done) {
+//       browser.visit('/users/new', done);
+//     });
+//
+//     before(function(done) {
+//       browser.fill('name', 'ewan', done);
+//       browser.fill('email', 'ewan', done);
+//       browser.fill('password', 'ewan', done);
+//       browser.fill('password_confirmation', 'ewan', done);
+//       browser.pressButton('Create Account', done);
+//     });
+//
+//     it('should not sign in without correctly formatted email', function() {
+//       browser.assert.url({pathname: '/users/new'});
+//     });
+//
+//   });
+//
+//   describe('mails for the users must be unique', function() {
+//     before(function(done) {
+//       browser.visit('/users/new', done);
+//     });
+//
+//     before(function(done) {
+//       browser.fill('name', 'ewan', done);
+//       browser.fill('email', 'ewan@alfie.com', done);
+//       browser.fill('password', 'ewan', done);
+//       browser.fill('password_confirmation', 'ewan', done);
+//       browser.pressButton('Create Account', done);
+//     });
+//     before(function(done) {
+//         browser.clickLink('Log out', done);
+//     });
+//     before(function(done) {
+//       browser.visit('/users/new', done);
+//     });
+//
+//     before(function(done) {
+//       browser.fill('name', 'ewan', done);
+//       browser.fill('email', 'ewan@alfie.com', done);
+//       browser.fill('password', 'ewan', done);
+//       browser.fill('password_confirmation', 'ewan', done);
+//       browser.pressButton('Create Account', done);
+//     });
+//
+//     it("should show an error message", function(){
+//       browser.assert.text("body", /This mail was used previously/);
+//     });
+//   });
+//
+//   describe('incorrect sign up - no name', function() {
+//
+//     before(function(done) {
+//       browser.visit('/users/new', done);
+//     });
+//
+//     before(function(done) {
+//       browser.fill('email', 'ewan@ewan.ewan', done);
+//       browser.fill('password', 'ewan', done);
+//       browser.fill('password_confirmation', 'ewan', done);
+//       browser.pressButton('Create Account', done);
+//     });
+//
+//     it('should not sign in without name', function() {
+//       browser.assert.url({pathname: '/users/new'});
+//     });
+//
+//     it("should show an error message", function(){
+//       browser.assert.text("body", /You need to enter a name, email and matching passwords/);
+//     });
+//   });
+//
+//   describe('incorrect sign up - not matching password', function() {
+//
+//     before(function(done) {
+//       browser.visit('/users/new', done);
+//     });
+//
+//     before(function(done) {
+//       browser.fill('name', 'ewan', done);
+//       browser.fill('email', 'ewan@ewan.ewan', done);
+//       browser.fill('password', 'ewan', done);
+//       browser.fill('password_confirmation', 'ewan2', done);
+//       browser.pressButton('Create Account', done);
+//     });
+//
+//     it('should not sign up with unmatching passwords', function() {
+//       browser.assert.url({pathname: '/users/new'});
+//     });
+//
+//     it("should show an error message", function(){
+//       browser.assert.text("body", /You need to enter a name, email and matching passwords/);
+//     });
+//   });
+//
+//   after(function(done) {
+//     server.close(done);
+//   });
+//
+// });
